1f174e35cc308013ec81b04b11842b63ef92c3e0
trainig2020 <68064760+trainig2020@users.noreply.github.com>
2020-09-01 00:00:00 +0530

    Add files via upload

---
+++ employee_service/src/test/java/io/prathyusha/employeeservice/service/EmployeeTestCases.java
@@ -0,0 +1,50 @@
+package io.prathyusha.employeeservice.service;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import io.prathyusha.employeeservice.model.Employee;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EmployeeTestCases {
+	
+	/*
+	 * @Autowired private EmployeeService employeeService;
+	 * 
+	 * @Test public void getAllEmployeeTest() { Employee employee = new Employee();
+	 * employeeService.getAllEmployess(); Assert.assertNotNull(employee);
+	 * 
+	 * }
+	 * 
+	 * @Test public void addEmployeeTest() {
+	 * 
+	 * Employee employee = new Employee(1, "Gagna",26,1);
+	 * employeeService.addEmployee(employee); Assert.assertNotNull(employee);
+	 * Assert.assertEquals("Gagna", employee.getEmpName());
+	 * 
+	 * }
+	 * 
+	 * @Test public void updateEmployeeTest() {
+	 * 
+	 * Employee employee = new Employee(7, "Nuthana",26,5);
+	 * employeeService.updateEmployee(7, employee); Assert.assertNotNull(employee);
+	 * Assert.assertEquals("Nuthana", employee.getEmpName());
+	 * 
+	 * }
+	 * 
+	 * 
+	 * 
+	 * @Test public void deleteEmployeeTest() { Employee employee = new Employee();
+	 * Assert.assertEquals(1, employee.getEmpId()); //
+	 * Assert.assertEquals(1,employee.getEmpId());
+	 * employeeService.deleteEmployeeByEdid(1,employee.getEmpId());
+	 * 
+	 * }
+	 */
+
+}
